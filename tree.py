17ab60bca9f7995ab9b0c468eb4a2445ea2e1280
Thina <Thina@zipcodersmbp42.mynetworksettings.com>
2022-07-12 21:28:44 -0400

    in progress

---
+++ tree.py
@@ -0,0 +1,19 @@
+import os
+
+def walk(dirname):
+    """Prints the names of all files in dirname and its subdirectories.
+    This is the version in the book.
+    dirname: string name of directory
+    """
+    for name in os.listdir(dirname):
+        path = os.path.join(dirname, name)
+        return path
+
+def writing_to_file(message):
+    with open('directory.txt', 'w') as f:
+        f.write(message)
+
+
+if __name__ == '__main__':
+    walk('.')
+    writing_to_file(walk('.'))
